4d9d1e467b5eac2f7ef2f1c2e4957546aaa33a81
Trifon Shterev <67158514+TrifShterev@users.noreply.github.com>
2020-07-19 17:59:17 +0300

    Add files via upload

---
+++ RegexBasics/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regexCode = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+            string names = Console.ReadLine();
+
+            MatchCollection matchedNames = regexCode.Matches(names);
+
+            foreach (Match name in matchedNames)
+            {
+                Console.Write(name.Value +" ");
+            }
+            
+        }
+    }
+}
